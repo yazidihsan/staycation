36d71188e5631afe2c4230c98115b30baf7fdce7
yazidihsan <yazidihsan1@gmail.com>
2024-12-11 15:15:04 +0700

    testing button component

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "assets/scss/style.scss";
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ src/pages/landingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+import moduleName from 'elements';
+export default class landingPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
